f9a321622e7cdd01c17413de5d57f57296445ffe
Sergio Bazan <drakeb2008@hotmail.com>
2023-12-19 21:34:35 -0500

    add exceptions

--- src/CleanArchitecture/CleanArchitecture.Application/Alquileres/ReservarAlquiler/ReservarAlquilerCommandHandler.cs
+++ src/CleanArchitecture/CleanArchitecture.Application/Alquileres/ReservarAlquiler/ReservarAlquilerCommandHandler.cs
@@ -1,5 +1,6 @@
 using CleanArchitecture.Application.Abstractions.Clock;
 using CleanArchitecture.Application.Abstractions.Messaging;
+using CleanArchitecture.Application.Exceptions;
 using CleanArchitecture.Domain.Abstractions;
 using CleanArchitecture.Domain.Alquileres;
 using CleanArchitecture.Domain.Users;
@@ -57,18 +58,26 @@ internal sealed class ReservarAlquilerCommandHandler
             return Result.Failure<Guid>(AlquilerErrors.Overlap);
         }
 
-        var alquiler = Alquiler.Reservar(
-            vehiculo,
-            user.Id,
-            duracion,
-            _dateTimeProvider.currentTime,
-            _precioService
-        );
+        try
+        {
+            var alquiler = Alquiler.Reservar(
+                vehiculo,
+                user.Id,
+                duracion,
+                _dateTimeProvider.currentTime,
+                _precioService
+            );
 
-        _alquilerRepository.Add(alquiler);
-        
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+            _alquilerRepository.Add(alquiler);
+            
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return alquiler.Id;
+            return alquiler.Id;
+        }
+        catch (ConcurrencyException)
+        {
+            return Result.Failure<Guid>(AlquilerErrors.Overlap);
+        }
+
     }
 }
--- src/CleanArchitecture/CleanArchitecture.Infrastructure/ApplicationDbContext.cs
+++ src/CleanArchitecture/CleanArchitecture.Infrastructure/ApplicationDbContext.cs
@@ -1,16 +1,58 @@
+using CleanArchitecture.Application.Exceptions;
 using CleanArchitecture.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : base(options) { }
+
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IPublisher publisher)
+     : base(options)
+    {
+        _publisher = publisher;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken =  default)
+    {
+        try 
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("La excepcion por concurrencia se disparo", ex);
+        }
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity => 
+            {
+                var domainEvents = entity.GetDomainEvents();
+                entity.ClearDomainEvents();
+                return domainEvents;
+            }).ToList();
+
+        foreach(var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
